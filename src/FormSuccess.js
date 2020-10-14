7dcef2e85b27b8f316f0a143881b6971d92215aa
Vaibhav Gupta <vaibhav16feb1997@gmail.com>
2020-10-14 13:09:34 +0530

    14oct13:09

---
+++ src/FormSuccess.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const FormSuccess = () => {
+    return (
+        <div class="form-content-right">
+            <div className="form-success">
+            We have recived your request!</div>
+            <img src="https://raw.githubusercontent.com/briancodex/react-form-v1/286f4a4603bda257ae001dc57c74d7f30bd4eedb/public/img/img-3.svg" alt="image" className="form-img-2"/>
+        </div>
+    )
+}
+
+export default FormSuccess
